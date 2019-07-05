5da50394ad3c0513d114417c04685b7adb7c8d56
johnwanjema <jonwanjema@gmail.com>
2019-07-05 14:18:11 +0300

    added blogs class

--- app/models.py
+++ app/models.py
@@ -1,22 +1,25 @@
 from . import db
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
 from . import login_manager
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(UserMixin,db.Model):
-    
+
+class User(UserMixin, db.Model):
+
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),index = True)
-    email = db.Column(db.String(255),unique = True,index = True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), index=True)
+    email = db.Column(db.String(255), unique=True, index=True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
+    blog = db.relationship('Blog', backref='user', lazy="dynamic")
     password_hash = db.Column(db.String(255))
 
     @property
@@ -27,10 +30,8 @@ class User(UserMixin,db.Model):
     def password(self, password):
         self.password_hash = generate_password_hash(password)
 
-
-    def verify_password(self,password):
-        return check_password_hash(self.password_hash,password)
-
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def save_user(self):
         db.session.add(self)
@@ -39,3 +40,33 @@ class User(UserMixin,db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer, primary_key=True)
+    blog_title = db.Column(db.String)
+    blog = db.Column(db.String)
+    blog_upvotes = db.Column(db.Integer)
+    blog_downvotes = db.Column(db.Integer)
+    posted = db.Column(db.Time, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comment_id = db.relationship("Comments", backref="blog", lazy="dynamic")
+
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_blog(cls, id):
+        blog = blog.query.filter_by(id=id).first()
+        return blog
+
+    @classmethod
+    def get_bloges(cls, id):
+        bloges = blog.query.filter_by(id=id).all()
+        return bloges
+
+    @classmethod
+    def get_bloges_by_category(cls, blog_category):
+        bloges = blog.query.filter_by(blog_category=blog_category).all()
+        return bloges
