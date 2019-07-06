98ca0c7ff3f432d8fce60cc81a52b80975cff0e6
johnwanjema <jonwanjema@gmail.com>
2019-07-06 18:09:51 +0300

    added profile image

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,13 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
-
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 mail = Mail()
+photos = UploadSet('photos',IMAGES)
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -23,6 +24,7 @@ def create_app(config_state):
     bootstrap.init_app(app)
     db.init_app(app)
     mail.init_app(app)
+    configure_uploads(app,photos)
     login_manager.init_app(app)
 
     from .main import main as main_blueprint
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for, abort, flash
 from . import main
 from ..email import mail_message
-from .. import db
+from .. import db,photos
 from ..requests import get_quotes
 from ..models import Blog, User, Comment
 from flask_login import login_user, logout_user, login_required, current_user
@@ -81,6 +81,18 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
+
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
 
 
 
